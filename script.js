f96d72dd3d5317426219376df2897e3a3a0f2a28
kumaradevisarath <kumaradevisarath2021@gmail.com>
2024-02-21 17:19:34 +0800

    update the file

---
+++ script.js
@@ -0,0 +1,38 @@
+
+
+var obtained = document.getElementById("obtained");
+var r1 = document.getElementById("r1");
+var r2 = document.getElementById("r2");
+var r3 = document.getElementById("r3");
+
+var box=document.getElementById("box");
+
+var btn = document.getElementById("btn");
+
+
+btn.addEventListener("click", function () {
+    var score = 0;
+    if (document.getElementById("r1").checked) {
+        score++;
+
+    }
+    else {
+        score;
+    }
+    if (document.getElementById("r2").checked) {
+        score++;
+    }
+    else {
+        score;
+    }
+    if (document.getElementById("r3").checked) {
+        score++
+    }
+    else {
+        score;
+    }
+
+    obtained.innerHTML = "you got:" + score;
+    alert("you completed the test and your score is:"+score)
+    box.innerHTML=`<h2>Interactive quiz</h2>`
+})
